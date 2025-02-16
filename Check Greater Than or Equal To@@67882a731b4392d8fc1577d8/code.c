8eb4b457f04de401f9db66901cd511ea95c3c879
Gurnoor Kaur <gurnoor0303.becse24@chitkara.edu.in>
2025-02-16 17:19:40 +0530

    Solution to Check Greater Than or Equal To

---
+++ Check Greater Than or Equal To@@67882a731b4392d8fc1577d8/code.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+    int a;
+    printf("Enter first number: ");
+    scanf("%d",&a);
+    int b;
+    printf("Enter second number:");
+    scanf("%d",&b);
+    if(a>=b) {
+        printf("True");
+    }
+    else printf("False");
+    return 0
+}

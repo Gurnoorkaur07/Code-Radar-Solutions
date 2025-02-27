259c04fd20da15904d196190d77c7d85377702fb
Gurnoor Kaur <gurnoor0303.becse24@chitkara.edu.in>
2025-02-27 19:36:01 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -6,7 +6,7 @@ int main(){
         printf("Digit");
     }
     else if((ch>='A'&&ch<='z')||(ch>='a'&&ch<='z')){
-        if(ch=='A'||ch='E'||ch='I'||ch='O'||ch='U'||ch='a'||ch='e'||ch='i'||ch='o'||ch='u'){
+        if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
             printf("Vowel");
         }
         else{

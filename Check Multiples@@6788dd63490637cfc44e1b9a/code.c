3bce6fd9dd1f9d75935f2e0be8b54fc40edc0dc2
Gurnoor Kaur <gurnoor0303.becse24@chitkara.edu.in>
2025-02-28 20:29:42 +0530

    Solution to Check Multiples

---
+++ Check Multiples@@6788dd63490637cfc44e1b9a/code.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int num,divisor;
+    scanf("%d %d",&num,&divisor);
+    if(num%divisor==0){
+        printf("Yes");
+    }else{
+        printf("No");
+    }
+    return 0;
+}

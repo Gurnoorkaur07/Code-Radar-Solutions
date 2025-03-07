a051bf756c3e07222114779da8ca2a6ee530a35c
Gurnoor Kaur <gurnoor0303.becse24@chitkara.edu.in>
2025-03-07 14:26:49 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    char grades;
+    scanf("%c",&grades);
+    if(grade==A){
+        printf("Excellent");
+    }else if(grade==B){
+        prinf("Good");
+    }else if(grade==C){
+        print("Average");
+    }else if(grade==D){
+        printf("Below Average");
+    }else if(grade==F){
+        prinf("Fail");
+    }else{
+        printf("Invalid");
+    }
+    return 0;
+}

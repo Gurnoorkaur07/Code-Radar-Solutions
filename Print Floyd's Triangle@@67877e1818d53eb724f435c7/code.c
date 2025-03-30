0cda73b78500cfee7e51e32e9fa74f08266a1620
Gurnoor Kaur <gurnoor0303.becse24@chitkara.edu.in>
2025-03-30 12:55:22 +0530

    Solution to Print Floyd's Triangle

--- Print Floyd's Triangle@@67877e1818d53eb724f435c7/code.c
+++ Print Floyd's Triangle@@67877e1818d53eb724f435c7/code.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
-int main() {
-    int n, num = 1;
-    scanf("%d", &n);
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= i; j++) {
-            printf("%d ", num++);
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter no. of rows: ");
+    scanf("%d",&n);
+    int a =1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("%d ",a);
+            a++;
         }
         printf("\n");
     }
